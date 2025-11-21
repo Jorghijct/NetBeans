24b532b23d37df65d428eaf6c0f0090e4c9fbb82
Jorghi Cote <jorghijct@gmail.com>
2025-11-21 19:50:54 -0300

    agg: función de carga automatica de clases desde archivos PHP

---
+++ class/autoload.php
@@ -0,0 +1,9 @@
+<?php
+/* @autor Jorghi Cote */
+spl_autoload_register(function ($class_name) {
+    $file = __DIR__ . '/' . $class_name . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+?>
